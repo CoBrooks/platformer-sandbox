c444e93138502c23112efadbf90e558ef69b8bd6
Cole Brooks <cole.brooks@colebrooks.com>
2026-01-13 20:53:30 -0600

    Moveable solids

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 CC := gcc
-CFLAGS := -Wall -Werror -ggdb -std=c99
+CFLAGS := -Wall -Werror -ggdb -std=c99 -O2
 LDFLAGS := -I./raylib-5.5_linux_amd64/include -L./raylib-5.5_linux_amd64/lib -l:libraylib.a -lm
 
 SRC_DIR := src
--- src/main.c
+++ src/main.c
@@ -1,3 +1,4 @@
+#include <math.h>
 #include <stdlib.h>
 #include <raylib.h>
 
@@ -40,16 +41,11 @@ typedef struct {
     u8 jump;
 } Input;
 
+// 2D axis-aligned bounding-box
 typedef struct {
     u16 x, y;
     u16 width, height;
-} Actor;
-
-typedef struct {
-    Actor *ptr;
-    u32    len;
-    u32    cap;
-} Actors;
+} AABB;
 
 typedef struct {
     u16 x, y;
@@ -65,7 +61,20 @@ typedef struct {
 } Solids;
 
 typedef struct {
-    Actor actor;
+    u16 x, y;
+    u16 width, height;
+
+    Solid *riding;
+} Actor;
+
+typedef struct {
+    Actor *ptr;
+    u32    len;
+    u32    cap;
+} Actors;
+
+typedef struct {
+    Actor *actor;
 
     f32 vx, vy; // velocity
     f32 ax, ay; // acceleration
@@ -82,6 +91,8 @@ typedef struct {
 // helpers //
 f32 clamp(f32 v, f32 min, f32 max);
 f32 sign(f32 f);
+
+bool aabb_overlaps(const AABB *a, const AABB *b);
 
 // physics //
 bool actor_move_x(Actor *actor, s16 dx, Solids solids);
@@ -127,6 +138,12 @@ f32 sign(f32 f)
     return f < 0.0 ? -1.0 : 1.0;
 }
 
+bool aabb_overlaps(const AABB *a, const AABB *b)
+{
+    return (a->x + a->width > b->x && a->x < b->x + b->width)
+        && (a->y + a->height > b->y && a->y < b->y + b->height);
+}
+
 // physics //
 bool actor_move_x(Actor *actor, s16 dx, Solids solids)
 {
@@ -139,11 +156,10 @@ bool actor_move_x(Actor *actor, s16 dx, Solids solids)
             Solid solid = solids.ptr[i];
             if (!solid.collidable) continue;
 
-            if (solid.x + solid.width <= actor->x || solid.x >= actor->x + actor->width) continue;
-            if (solid.y + solid.height <= actor->y || solid.y >= actor->y + actor->height) continue;
-
-            actor->x -= dx_sign;
-            return true;
+            if (aabb_overlaps((AABB*)&actor->x, (AABB*)&solid.x)) {
+                actor->x -= dx_sign;
+                return true;
+            }
         }
     }
 
@@ -161,11 +177,11 @@ bool actor_move_y(Actor *actor, s16 dy, Solids solids)
             Solid solid = solids.ptr[i];
             if (!solid.collidable) continue;
 
-            if (solid.x + solid.width <= actor->x || solid.x >= actor->x + actor->width) continue;
-            if (solid.y + solid.height <= actor->y || solid.y >= actor->y + actor->height) continue;
-
-            actor->y -= dy_sign;
-            return true;
+            if (aabb_overlaps((AABB*)&actor->x, (AABB*)&solid.x)) {
+                actor->y -= dy_sign;
+                actor->riding = &solids.ptr[i];
+                return true;
+            }
         }
     }
 
@@ -174,28 +190,86 @@ bool actor_move_y(Actor *actor, s16 dy, Solids solids)
 
 void solid_move(Solid *solid, s16 dx, s16 dy, Actors actors)
 {
-    // TODO
+    // nothing to do
+    if (dx == 0 && dy == 0) return;
+
+    // tmp disable collision
+    solid->collidable = false;
+
+    if (dx != 0) {
+        solid->x += dx;
+
+        for (u64 i = 0; i < actors.len; ++i) {
+            Actor actor = actors.ptr[i];
+
+            if (aabb_overlaps((AABB*)&actor.x, (AABB*)&solid->x)) {
+                s16 difference = dx > 0
+                    ? solid->x + solid->width - actor.x
+                    : solid->x - (actor.x + actor.width);
+
+                // push
+                if (actor_move_x(&actors.ptr[i], difference, solids)) {
+                    // TODO: squish
+                    TraceLog(LOG_WARNING, "Squish!");
+                }
+            } else if (actor.riding == solid) {
+                // carry
+                actor_move_x(&actors.ptr[i], dx, solids);
+            }
+        }
+    }
+
+    if (dy != 0) {
+        solid->y += dy;
+
+        for (u64 i = 0; i < actors.len; ++i) {
+            Actor actor = actors.ptr[i];
+
+            if (aabb_overlaps((AABB*)&actor.x, (AABB*)&solid->x)) {
+                s16 difference = dy > 0
+                    ? solid->y + solid->height - actor.y
+                    : solid->y - (actor.y + actor.height);
+
+                // push
+                if (actor_move_y(&actors.ptr[i], difference, solids)) {
+                    // TODO: squish
+                    TraceLog(LOG_WARNING, "Squish!");
+                }
+            } else if (actor.riding == solid) {
+                // carry
+                actor_move_y(&actors.ptr[i], dy, solids);
+            }
+        }
+    }
+
+    solid->collidable = true;
 }
 
 // main event loop //
 void init(void)
 {
+    Actor player_actor = {
+        .x = 400,
+        .y = 300,
+        .width = 10,
+        .height = 10,
+    };
+
+    da_append(actors, player_actor);
+
     player = (Player){
-        .actor = (Actor){
-            .x = 400,
-            .y = 300,
-            .width = 10,
-            .height = 10,
-        },
+        .actor = &actors.ptr[0],
         .speed = 1.0,
         .jump_speed = 15.0,
         .ay = GRAVITY,
     };
 
     const Solid level[] = {
-        { 100, 400, 600, 10, true },
-        { 100, 300, 10, 100, true },
-        { 300, 350, 200, 10, true },
+        { 100, 400, 600,  10, true },
+        { 100, 300,  10, 100, true },
+        { 300, 350, 200,  10, true },
+        { 300, 350, 100,  10, true }, // moves vertically
+        { 400, 300,  50,  10, true }, // moves horizontally
     };
 
     for (u64 i = 0; i < sizeof(level) / sizeof(*level); ++i) {
@@ -208,10 +282,11 @@ void update(void)
     if (tick++ % 120 == 0) {
         TraceLog(
             LOG_INFO,
-            "Player pos:(%d, %d) vel:(%f, %f) acc:(%f, %f) grounded:%s",
-            player.actor.x,  player.actor.y,
+            "Player pos:(%d, %d) vel:(%f, %f) acc:(%f, %f) riding:%p grounded:%s",
+            player.actor->x,  player.actor->y,
             player.vx, player.vy,
             player.ax, player.ay,
+            player.actor->riding,
             player.grounded ? "true" : "false"
         );
     }
@@ -227,19 +302,42 @@ void update(void)
 
     if (IsKeyPressed(KEY_UP) || IsKeyPressed(KEY_W) || IsKeyPressed(KEY_SPACE)) input.jump = true;
 
+    // level update //
+    {
+        static s16 dir = 1;
+        Solid *block = &solids.ptr[3];
+
+        if ((block->y - 375) % 100 == 0) {
+            dir *= -1;
+        }
+
+        solid_move(block, 0, dir, actors);
+    }
+    {
+        static s16 dir = 1;
+        Solid *block = &solids.ptr[4];
+
+        if ((block->x - 375) % 100 == 0) {
+            dir *= -1;
+        }
+
+        solid_move(block, dir, 0, actors);
+    }
+
     // player update //
     player.ax = player.speed * input.h;
 
     if (input.jump && player.grounded) player.vy = -player.jump_speed;
 
     player.vx = clamp(player.vx + player.ax, -TOP_SPEED, TOP_SPEED);
-    actor_move_x(&player.actor, player.vx, solids);
+    actor_move_x(player.actor, player.vx, solids);
 
     // Without this, turning around feels "slippery" since the player is accelerating from a speed < 0
     if (sign(player.vx) != sign(input.h) || input.h == 0.0) player.vx = 0.0;
 
     player.vy = player.vy + player.ay;
-    player.grounded = actor_move_y(&player.actor, player.vy, solids);
+    player.actor->riding = NULL;
+    player.grounded = actor_move_y(player.actor, player.vy, solids);
 
     if (player.grounded) player.vy = 0.0;
 }
@@ -253,7 +351,7 @@ void draw(void)
         DrawRectangle(solid.x, solid.y, solid.width, solid.height, DARKGRAY);
     }
 
-    DrawRectangle(player.actor.x, player.actor.y, player.actor.width, player.actor.height, RED);
+    DrawRectangle(player.actor->x, player.actor->y, player.actor->width, player.actor->height, RED);
 }
 
 void deinit(void)
